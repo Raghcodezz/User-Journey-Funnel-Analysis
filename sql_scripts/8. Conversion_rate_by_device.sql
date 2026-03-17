673ed6728aa1b1b1d1910698ae583aacb17cb4bf
P. Shiva Raghavendra Kumar <raghav113222@gmail.com>
2026-03-17 22:10:23 +0530

    Create 8. Conversion_rate_by_device.sql

---
+++ sql_scripts/8. Conversion_rate_by_device.sql
@@ -0,0 +1,8 @@
+select 
+devicetype,
+referralsource,
+cast(round((sum(purchased)*100.0/count(*)),2)
+as decimal(10,2)) as conversion_rate
+from ecommerce_journey
+group by devicetype, referralsource
+order by devicetype
